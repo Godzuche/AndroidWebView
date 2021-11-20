c49f82a7957957b0234e487c781a3b20e40f71ba
Administrator <godxuche@gmail.com>
2021-11-20 06:59:11 +0100

    Initial commit with viewBinding enabled

---
+++ app/src/main/java/com/godzuche/androidwebview/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.godzuche.androidwebview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.godzuche.androidwebview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
